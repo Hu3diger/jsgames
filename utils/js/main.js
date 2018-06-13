fd957aca5a1bf802c7eada2d00b9cc7048cb8435
AltieriR <altidala@hotmail.com>
2018-06-12 22:04:31 -0300

    8 Queens started

---
+++ utils/js/main.js
@@ -0,0 +1,27 @@
+(function(){
+	/*Diagonal
+	i+1 ou i-1 e j+2 ou j-2
+	YO FRONTE MEIN
+	todos i
+	Lados
+	todos j*/
+	
+	var posicaoSelecionada;
+	$rows = document.getElementsByTagName("tr");
+	$cols = document.getElementsByTagName("td");
+	$peca = document.createElement("img");
+	
+	function padrao(){
+		
+	}
+	
+	function base(){
+		
+	}
+	
+	function pontas(){
+		
+	}
+
+
+)();
