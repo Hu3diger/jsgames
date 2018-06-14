261fe51e37f323d93b0e9f471cd58d40e041931c
AltieriR <altidala@hotmail.com>
2018-06-13 21:16:14 -0300

    Add queen function

--- utils/js/main.js
+++ utils/js/main.js
@@ -7,9 +7,15 @@
 	todos j*/
 	
 	var posicaoSelecionada;
-	$rows = document.getElementsByTagName("tr");
-	$cols = document.getElementsByTagName("td");
-	$peca = document.createElement("img");
+	var $tabela = document.getElementsByTagName("tbody")[0];
+	var $rows = document.getElementsByTagName("tr");
+	var $cols = document.getElementsByTagName("td");
+	var $peca = document.createElement("img");
+	
+	$tabela.addEventListener('click', function(e){
+		console.log(e.target);
+		addQueenImageToTableCell(e.target);
+	});
 	
 	function padrao(){
 		
@@ -23,5 +29,21 @@
 		
 	}
 
+	function addQueenImageToTableCell(celula) {
+		var queenImage = document.createElement('img');
+		queenImage.src = "utils/images/queen.png";
+		queenImage.classList.add("queen");
+		queenImage.style.width='50px';
+		queenImage.style.height='50px';
+		if (celula.hasChildNodes()) {
+			console.log("Removendo filho");
+			celula.removeChild(celula.lastChild);
+			debugger;
+		} else {
+			console.log("Adicionando filho");
+			celula.appendChild(queenImage);
+		}
+	}
+}
 
 )();
