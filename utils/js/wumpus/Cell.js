ef0755c3cda8bc51ad75395259ca9d93a88b0421
Esdras Xavier <esdrasxa@gmail.com>
2018-06-17 00:10:03 -0300

    Jogo wumpus quase pronto, faltando apenas configurar textura e verificar alguns detalhes com a equipe

--- utils/js/wumpus/Cell.js
+++ utils/js/wumpus/Cell.js
@@ -1,12 +1,16 @@
 
-var Cell = function(i, j, w, hasHole) {
+var Cell = function(i, j, w, hasHole, hasWumpus) {
 
   this.x = i;
   this.y = j;
   this.width = w;
 
-  this.hasWumpus = false;
+  this.hasWumpus = hasWumpus;
   this.hasHole = hasHole;
+
+  this.visited = true;
+  this.closeHole = false;
+  this.closeWumpus = false;
 }
 
 Cell.prototype.show = function() {
@@ -15,9 +19,79 @@ Cell.prototype.show = function() {
   let x = this.x * w;
   let y = this.y * w;
 
-  stroke(255);
-  noFill();
+  if (this.visited == false) {
+    stroke(255);
+
+    if (this.hasHole == true)
+      fill(0);
+    else if (this.hasWumpus == true)
+      fill(255);
+    else if (this.closeHole == true)
+      fill(255, 0, 0);
+    else if (this.closeWumpus == true)
+      fill(0, 255, 0);
+    else
+      noFill();
+  } else {
+    noStroke();
+    fill(51);
+  }
+
   rect(x, y, w, w);
+}
+
+Cell.prototype.showNeighborsWumpus = function() {
+  let w = this.width;
+  let x = this.x;
+  let y = this.y;
+
+  let top     = grid[index(x    , y - 1)];
+  let left    = grid[index(x - 1, y    )];
+  let bottom  = grid[index(x + 1, y    )];
+  let rigth   = grid[index(x    , y + 1)];
+
+  if (top && top.hasHole == false) {
+    top.closeWumpus = true;
+  }
+
+  if (left && left.hasHole == false) {
+    left.closeWumpus = true;
+  }
+
+  if (bottom && bottom.hasHole == false) {
+    bottom.closeWumpus = true;
+  }
+
+  if (rigth && rigth.hasHole == false) {
+    rigth.closeWumpus = true;
+  }
+}
+
+Cell.prototype.showNeighbors = function () {
+  let w = this.width;
+  let x = this.x;
+  let y = this.y;
+
+  let top     = grid[index(x    , y - 1)];
+  let left    = grid[index(x - 1, y    )];
+  let bottom  = grid[index(x + 1, y    )];
+  let rigth   = grid[index(x    , y + 1)];
+
+  if (top && top.hasHole == false) {
+    top.closeHole = true;
+  }
+
+  if (left && left.hasHole == false) {
+    left.closeHole = true;
+  }
+
+  if (bottom && bottom.hasHole == false) {
+    bottom.closeHole = true;
+  }
+
+  if (rigth && rigth.hasHole == false) {
+    rigth.closeHole = true;
+  }
 }
 
 Cell.prototype.getHole = function() {
--- utils/js/wumpus/wumpusGame.js
+++ utils/js/wumpus/wumpusGame.js
@@ -3,7 +3,6 @@
  * Fonts: https://en.wikipedia.org/wiki/Hunt_the_Wumpus
  */
 
-
 var loading = false, gameStarted = false;
 var loadingCounter = 0;
 var pages, actualPage = "loading";
@@ -11,30 +10,21 @@ var w = 50;
 var row, col;
 var grid = [];
 var char;
+var current;
+var lose = false;
+
+$().ready(function() {
+  $(".modal").modal();
+
+  $(".reset").on("click", function() {
+    resetGame()
+  });
+});
 
 function setup() {
   /* Create the canvas to draw */
   createCanvas(801, 501);
-  row = floor(width / w);
-  col = floor(height / w);
-
-  console.log("Possuo ", col, " colunas");
-  console.log("Possuo ", row, " linhas");
-
-  for (var x = 0; x < row; x++) {
-    for (var y = 0; y < col; y++) {
-      let a = random(50);
-      let cell = new Cell(x, y, w, (a > 48 ? true : false));
-      grid.push(cell);
-    }
-  }
-
-  console.log(grid);
-  pages = new NavPages();
-  pages.setPage("startPage");  // startPage   loading
-
-  char = new Char(1, 1, w);
-
+  resetGame();
 }
 
 function draw() {
@@ -67,10 +57,58 @@ function draw() {
       grid[x].show();
     }
 
+    for (var x = 0; x < grid.length; x++) {
+      if (grid[x].hasHole == true) {
+        current = grid[x];
+        current.showNeighbors();
+      } else if (grid[x].hasWumpus == true) {
+        current = grid[x];
+        current.showNeighborsWumpus();
+      }
+    }
+
     char.showChar();
 
   }
 }
+
+
+function resetGame() {
+  lose = false;
+
+  row = floor(width / w);
+  col = floor(height / w);
+  let useWumpus = false;
+  let wumpusUsed = false;
+  let hole;
+
+  for (var x = 0; x < row; x++) {
+    for (var y = 0; y < col; y++) {
+
+      if (wumpusUsed == false && x > 5) {
+        useWumpus = (random(50) > 45 ? true : false);
+        if (useWumpus) wumpusUsed = true;
+      } else {
+        useWumpus = false;
+      }
+
+      let a = random(50);
+      let useHole = false;
+      if (x > 0) useHole = (a > 48 ? true : false);
+      // if (x == 3 && y == 5) useHole = true;
+
+      let cell = new Cell(x, y, w, useHole, useWumpus);
+      grid.push(cell);
+    }
+  }
+
+  pages = new NavPages();
+  pages.setPage("startPage");  // startPage   loading
+
+  char = new Char(1, 1, w);
+  grid[0].visited = false;
+}
+
 
 function keyPressed() {
 
@@ -78,43 +116,98 @@ function keyPressed() {
     pages.setPage("loading");
     gameStarted = true;
     loading = true;
-  } else {
+  } else if (lose == false) {
 
-
-    if (keyCode == UP_ARROW) { // top
+    if (keyCode == UP_ARROW) {  // top
       if (char.canMoveTo(0) == true) {
         char.moveTo(0, -2);
-      } else {
-        char.setDir(0)
       }
+      char.setDir(0)
     } else if (keyCode == LEFT_ARROW) { // left
       if (char.canMoveTo(1) == true) {
         char.moveTo(-2, 0);
-      } else {
-        char.setDir(1)
       }
+        char.setDir(1)
     } else if (keyCode == DOWN_ARROW) { // down
       if (char.canMoveTo(2) == true) {
         char.moveTo(0, 2);
-      } else {
-        char.setDir(2)
       }
+      char.setDir(2)
     } else if (keyCode == RIGHT_ARROW) { // right
       if (char.canMoveTo(3) == true) {
         char.moveTo(2, 0);
-      } else {
-        char.setDir(3)
+      }
+      char.setDir(3)
+    } else if (keyCode == 32) {
+      for (var x = 0; x < char.side.length; x++) {
+        if (char.side[x] == true) shoot(x);
       }
     }
 
     let charX = floor(char.x / 2);
     let charY = floor(char.y / 2);
+
     for (var x = 0; x < grid.length; x++) {
-      // grid[x].show();
       if (grid[x].x == charX && grid[x].y == charY) {
-        if (grid[x].hasHole == true) console.log("buraco");
-        else console.log("nope");
+        grid[x].visited = false;
+        if (grid[x].hasHole == true) {
+          $("#modalTitle").html("Você perdeu!");
+          $("#textModal").html("Você caiu dentro de um buraco :(");
+          $("#modal1").modal("open");
+          lose = true;
+        } else if(grid[x].hasWumpus == true) {
+          $("#modalTitle").html("Você perdeu!");
+          $("#textModal").html("O wumpus te comeu!");
+          $("#modal1").modal("open");
+          lose = true;
+        }
       }
     }
   }
+}
+
+function shoot(dir) {
+  let x = floor(char.x / 2);
+  let y = floor(char.y / 2);
+
+  let top     = grid[index(x    , y - 1)];
+  let left    = grid[index(x - 1, y    )];
+  let rigth   = grid[index(x + 1, y    )];
+  let bottom  = grid[index(x    , y + 1)];
+
+  let win = false;
+
+  if (dir == 0) {
+    if (top && top.hasWumpus) win = true
+
+  } else if (dir == 1) {
+    if (left && left.hasWumpus) win = true;
+
+  } else if (dir == 2) {
+    if (bottom && bottom.hasWumpus) win = true;
+
+  } else if (dir == 3) {
+    if (rigth && rigth.hasWumpus) win = true;
+  }
+
+  lose = true;
+  if (win == true) {
+    $("#modalTitle").html("Você ganhow!");
+    $("#textModal").html("Acertou o wumpus em cheio");
+    $("#modal1").modal("open");
+    return false;
+  }
+
+  $("#modalTitle").html("Você perdeu!");
+  $("#textModal").html("O local onde você atirou a flecha não possuia um Wumpus");
+
+  $("#modal1").modal("open");
+}
+
+
+function index(y, x) {
+  if (x < 0 || y < 0 || x > col-1 || y > row-1)
+    return -1;
+
+  return x + y * col;
 }
